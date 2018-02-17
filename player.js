01053a8d0dec5c6ebb7615760fc44f664093c286
MattScuteri <mttscuteri@gmail.com>
2018-02-17 11:59:44 -0500

    edit to player constructor

--- player.js
+++ player.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const Letter = require('./letter.js');
 const Word = require('./word.js');
 
+//build player constructor and put inquirer prompt after, calling functions that way
+
 function Player(wordLetters, guessedLetters, matchedLetters, totalGuesses) {
 	this.wordLetters = [];
 	this.guessedLetters = [];
@@ -20,43 +22,46 @@ function Player(wordLetters, guessedLetters, matchedLetters, totalGuesses) {
 			type: "input",
 
 		}]).then(function(response) {
-			response.guesses();
+			console.log(response);
+
+			response.addMatchLetters = function() {
+				for (let i = 0; i < this.wordLength.length; i++) {
+					if ((userLetter === this.wordLength[i]) && (this.matchedLetters.indexOf(userLetter) === -1)) {
+					this.matchedLetters.push(userLetter)
+					console.log(this.matchedLetters);
+					wordView(response);
+					};
+				};
+			};
+			response.wordView = function(response) {
+				let blankArray = "";
+				for (let i = 0; i < this.wordLength.length; i++) {
+					if(this.matchedLetters.indexOf(this.wordLength[i]) !== -1) {
+					blankArray += this.wordLength[i];
+					console.log(blankArray);
+					};
+				};
+			};
+			response.updateWins = function() {
+				let win;
+				if (this.matchedLetters.length === 0) {
+					win = false;
+				} else {
+					win = true;
+				};
+				for (let i = 0; i < this.wordLength.length; i++) {
+					if (this.matchedLetters.indexOf(this.wordLength[i]) === -1) {
+					win = false
+				};
+			};
+				if (win) {
+					this.wins = this.wins +1;
+					console.log("WINS: " + this.wins);
+				}
+			};
+
 		})
 	
-	};
-	this.addMatchLetters = function(userLetter) {
-		for (let i = 0; i < this.wordLength.length; i++) {
-			if ((userLetter === this.wordLength[i]) && (this.matchedLetters.indexOf(userLetter) === -1)) {
-				this.matchedLetters.push(userLetter)
-				console.log(this.matchedLetters);
-			};
-		};
-	};
-	this.wordView = function(userLetter) {
-		let blankArray = "";
-		for (let i = 0; i < this.wordLength.length; i++) {
-			if(this.matchedLetters.indexOf(this.wordLength[i]) !== -1) {
-				blankArray += this.wordLength[i];
-				console.log(blankArray);
-			};
-		};
-	};
-	this.updateWins = function() {
-		let win;
-		if (this.matchedLetters.length === 0) {
-			win = false;
-		} else {
-			win = true;
-		};
-		for (let i = 0; i < this.wordLength.length; i++) {
-			if (this.matchedLetters.indexOf(this.wordLength[i]) === -1) {
-				win = false
-			};
-		};
-		if (win) {
-			this.wins = this.wins +1;
-			console.log("WINS: " + this.wins);
-		}
 	};
 };
 
