3a4d08f99550ba459fdf344a842bbc9203bca8a4
MattScuteri <mttscuteri@gmail.com>
2018-02-18 11:47:26 -0500

    move inquirer prompt out of constructor

--- player.js
+++ player.js
@@ -5,68 +5,72 @@ const Word = require('./word.js');
 //build player constructor and put inquirer prompt after, calling functions that way
 
 function Player(wordLetters, guessedLetters, matchedLetters, totalGuesses) {
-	this.wordLetters = [];
-	this.guessedLetters = [];
-	this.matchedLetters = [];
-	this.totalGuesses = 15;
-	this.startGame = function() {
+	this.startGame = function(response) {
 		this.wordLetters = [];
 		this.guessedLetters = [];
 		this.matchedLetters = [];
 		this.totalGuesses = 15;
+		addMatchLetters(response);
 
-		let prompt = inquirer.prompt([
-		{
-			name: "userLetter",
-			message: "Select a letter!",
-			type: "input",
-
-		}]).then(function(response) {
-			console.log(response);
-
-			response.addMatchLetters = function() {
-				for (let i = 0; i < this.wordLength.length; i++) {
-					if ((userLetter === this.wordLength[i]) && (this.matchedLetters.indexOf(userLetter) === -1)) {
-					this.matchedLetters.push(userLetter)
-					console.log(this.matchedLetters);
-					wordView(response);
-					};
-				};
+	this.addMatchLetters = function(response) {
+		for (let i = 0; i < this.wordLength.length; i++) {
+			if ((userLetter === this.wordLength[i]) && (this.matchedLetters.indexOf(userLetter) === -1)) {
+				this.matchedLetters.push(userLetter)
+				console.log(this.matchedLetters);
+				wordView(response);
 			};
-			response.wordView = function(response) {
-				let blankArray = "";
-				for (let i = 0; i < this.wordLength.length; i++) {
-					if(this.matchedLetters.indexOf(this.wordLength[i]) !== -1) {
-					blankArray += this.wordLength[i];
-					console.log(blankArray);
-					};
-				};
-			};
-			response.updateWins = function() {
-				let win;
-				if (this.matchedLetters.length === 0) {
-					win = false;
-				} else {
-					win = true;
-				};
-				for (let i = 0; i < this.wordLength.length; i++) {
-					if (this.matchedLetters.indexOf(this.wordLength[i]) === -1) {
-					win = false
-				};
-			};
-				if (win) {
-					this.wins = this.wins +1;
-					console.log("WINS: " + this.wins);
-				}
-			};
-
-		})
-	
+		};
 	};
+	this.wordView = function(response) {
+		let blankArray = "";
+		for (let i = 0; i < this.wordLength.length; i++) {
+			if(this.matchedLetters.indexOf(this.wordLength[i]) !== -1) {
+				blankArray += this.wordLength[i];
+				console.log(blankArray);
+				updateWins(response);
+			};
+		};
+	};
+	this.updateWins = function(response) {
+		let win;
+			if (this.matchedLetters.length === 0) {
+				win = false;
+			} else {
+				win = true;
+			};
+			for (let i = 0; i < this.wordLength.length; i++) {
+				if (this.matchedLetters.indexOf(this.wordLength[i]) === -1) {
+					win = false
+			};
+			};
+			if (win) {
+				this.wins = this.wins +1;
+				console.log("WINS: " + this.wins);
+			}
+			};
+
+	}
+	
 };
 
-const userLetter = new Player();
+let prompt = inquirer.prompt([
+	{
+		name: "userLetter",
+		message: "Select a letter!",
+		type: "input",
 
-userLetter.startGame();
+	}]).then(function(response) {
+		const userLetter = response;
+
+		console.log(userLetter);
+
+		for (let i = 0; i < userLetter.length; i++) {
+			userLetter[i].startGame();
+			userLetter[i].matchedLetters();
+			userLetter[i].wordView();
+			userLetter[i].updateWins();
+		}
+		
+	})
 
 module.exports = Player;
